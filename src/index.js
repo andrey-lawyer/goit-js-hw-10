c2aff0ac82af6ba79438208de3f8306659400894
Andrey Uvarov <andrey-1@ukr.net>
2022-11-09 21:32:50 +0200

    exercise_js_vers2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+inputEl.addEventListener('input', debounce(onInput, 300));
+
+function onInput(event) {
+  const value = event.target.value;
+  // console.log(value.trim());
+  if (value.trim() == '') {
+    return;
+  }
+  fetchCountries(value.trim());
+}
